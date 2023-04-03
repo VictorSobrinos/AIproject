c59bbf08c39a3b9e4dcd7b4fd4c688cc3cc591d0
Víctor <victorsobrinos@gmail.com>
2023-04-03 20:09:26 +0200

    First configuration

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 //import Image from 'next/image'
 //import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+//import styles from '@/styles/Home.module.css'
 
 //const inter = Inter({ subsets: ['latin'] })
 
@@ -14,8 +14,8 @@ export default function Home() {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <main className={styles.main}>
-                <h1>
+            <main className='bg-black h-screen w-screen'>
+                <h1 className='text-white'>
                     Hello world!
                 </h1>
             </main>
